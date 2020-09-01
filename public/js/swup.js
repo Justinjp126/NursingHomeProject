3f4dffe44298f34e840b3b86fdca5d90587dec0c
Justinjp126 <39039227+Justinjp126@users.noreply.github.com>
2020-09-01 11:52:15 -0400

    Finish register page

--- public/js/swup.js
+++ public/js/swup.js
@@ -73,14 +73,17 @@ const hoverNavEffects = () => {
 
     for (var i = 0; i < links.length; i++) {
         links[i].addEventListener("click", (evt) => {
-            var target = evt.target || evt.srcElement;
-            target.classList.remove("not-active", "current");
-            target.classList.add("active", "current");
-
+            if(links[i].id !== "loginButton" && links[i].id !== "registerButton") {
+                var target = evt.target || evt.srcElement;
+                target.classList.remove("not-active", "current");
+                target.classList.add("active", "current");
+            } 
             for (var j = 0; j < links.length; j++) {
                 if (
                     !links[j].classList.contains("current") &&
-                    links[j].classList.contains("active")
+                    links[j].classList.contains("active") && 
+                    links[j].id !== "loginButton" &&
+                     links[j].id !== "registerButton"
                 ) {
                     links[j].classList.remove("active");
                     links[j].classList.add("not-active");
